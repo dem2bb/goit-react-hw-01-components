a99bf31885d0d9fc18bf8c7ccd238047eb4ac8db
Maxim Kazachuk <dem2bb1@gmail.com>
2021-02-22 19:02:33 +0200

    correction

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -46,9 +46,9 @@ Profile.propTypes = {
   tag: PropTypes.string,
   location: PropTypes.string,
   stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
   }),
 };
 
